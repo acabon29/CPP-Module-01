1398ed59603c9ee1eb17cc6fabcfc15460e87290
acabon <votre.email@example.com>
2025-07-10 17:55:56 +0200

    update ex02

--- ex02/srcs/main.cpp
+++ ex02/srcs/main.cpp
@@ -6,17 +6,19 @@
 
 int main( void )
 {
+	// initialisation :
 	std::string str = "HI THIS IS BRAIN";
-
 	std::string *stringPTR = &str; 
 	std::string &stringREF = str;
 
+	// adresse :
 	std::cout << &str << std::endl;
 	std::cout << stringPTR << std::endl;
-	std::cout << stringREF << std::endl;
+	std::cout << &stringREF << std::endl;
 
-	std::cout << &str << std::endl;
-	std::cout << stringPTR << std::endl;
+	// valeur :
+	std::cout << str << std::endl;
+	std::cout << *stringPTR << std::endl;
 	std::cout << stringREF << std::endl;
 	
 
